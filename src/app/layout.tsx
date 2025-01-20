be384defa984d9e41bad5445e4a376c783af7145
Nikke Leskelä <nikke.leskela@loikka.io>
2025-01-20 20:55:13 +0200

    favicon päivitys

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import Footer from "@/app/_components/footer";
-import { HOME_OG_IMAGE_URL, SITE_TITLE } from "@/lib/constants";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SITE_TITLE, HOME_OG_IMAGE_URL } from "@/lib/constants";
+import type { Metadata } from "next";
 
 import "./globals.css";
-import Header from "./_components/header";
+import HeaderWrapper from "./_components/header-wrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,39 +25,25 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/favicon/apple-touch-icon.png"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
+        <meta name="apple-mobile-web-app-title" content="Turun karjalakuoro" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#000000"
-        />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#000000" />
-        <meta
-          name="msapplication-config"
-          content="/favicon/browserconfig.xml"
-        />
         <meta name="theme-color" content="#000" />
-        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+        {/* <link rel="alternate" type="application/rss+xml" href="/feed.xml" /> */}
       </head>
       <body className={inter.className}>
-        <Header /> 
+        <HeaderWrapper />
         <div className="min-h-screen pt-20">{children}</div>
         <Footer />
       </body>
